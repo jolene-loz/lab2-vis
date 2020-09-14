37685928910b6345b7ba6b07250ac4e9d6c089d9
Jolene Lozano <lozanoja@bc.edu>
2020-09-14 16:17:50 -0400

    CSS modifications

---
+++ script.js
@@ -0,0 +1,34 @@
+
+let attractions;
+fetch('attractions.json')
+    .then(response => response.json())
+    .then(data => {
+        attractions = data;
+
+        function filterData(category) {
+            const maxLength = 5;
+
+            if(category == 'all'){
+                attractions.sort(function (a,b){
+                    return a.visitors > b.visitors;
+                });
+                let data = attractions.slice(0,5);
+                renderBarChart(data);
+            } else {
+                let filteredAttraction = attractions.filter(attractions => attractions.Category == category)
+                filteredAttraction.sort((a,b) => {
+                    return a.visitors > b.visitors;
+                });
+                let data = filteredAttraction.slice(0,maxLength);
+                renderBarChart(data);
+            } 
+        }
+
+        let selector = document.querySelector("#attraction-category");
+        selector.addEventListener('change', function(event){
+            attractions.filter(filterData(event.target.value));
+            console.log(event.target.value);
+        })
+    });
+
+
